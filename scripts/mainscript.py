2c9849d57c7293274353352b508a5e277cae6fd5
Landon DeCoito <rdecoito@gmu.edu>
2018-08-20 15:45:33 -0400

    scheduled cache for 8 PM every day and organised sources into a list of tuples for easy addition later

--- scripts/mainscript.py
+++ scripts/mainscript.py
@@ -1,17 +1,37 @@
-from requests import get
-import schedule
+# python imports
 import datetime
+import os
 from time import sleep
 
+# package imports
+from requests import get
+import schedule
 
 
-# def cache_the_cache():
-    # 25live = get("https://masontoday.gmu.io/api/25live").text
-    # getconn = get("https://masontoday.gmu.io/api/getconnected").text
+# current working directory
+currentdir = os.getcwd()
+
+# all the current MasonToday sources in format (filename, URL)
+sources = [
+    ("25Live", "https://masontoday.gmu.io/api/25live"),
+    ("GetConn", "https://masontoday.gmu.io/api/getconnected")
+]
 
 
-with open("../logs/MasonTodayLog_" + str(datetime.date.today()) + ".txt",
-          "w") as file:
-    file.write(get("https://masontoday.gmu.io/api/25live").text)
+def cache_sources():
+    for source in sources:
+        path = os.path.join(currentdir + "/logs/MasonTodayLog_" + source[0] \
+                                + "_" + str(datetime.date.today()) + ".txt")
+        with open(path, "a") as file:
+            file.write(str(get(source[1]).text))
+            file.close()
+        print(source[0] + " cache successful!")
 
-print("Woah, we did it!")
+
+# cache the sources at 8 PM every day
+schedule.every().day.at("20:00").do(cache_sources)
+
+do_it = True
+while (do_it):
+    schedule.run_pending()
+    sleep(3600)  # check every hour
